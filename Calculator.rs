2a52034f62b03fe3a4756b4d461fdfc267ea4270
VENKAT RAMANAN B <79049009+VENKAT-RAMANAN-B@users.noreply.github.com>
2023-08-18 00:01:38 +0530

    Calculator.rs

--- Calculator.rs
+++ Calculator.rs
@@ -12,7 +12,13 @@ fn main() {
 
         let mut choice = String::new();
         io::stdin().read_line(&mut choice).expect("Failed to read line");
-        let choice: u32 = choice.trim().parse().expect("Invalid choice");
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid choice. Please enter a number.");
+                continue;
+            }
+        };
 
         if choice == 5 {
             println!("Exiting the calculator.");
@@ -22,25 +28,52 @@ fn main() {
         println!("Enter the first number: ");
         let mut num1 = String::new();
         io::stdin().read_line(&mut num1).expect("Failed to read line");
-        let num1: f64 = num1.trim().parse().expect("Invalid number");
+        let num1: f64 = match num1.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number. Please enter a valid number.");
+                continue;
+            }
+        };
 
         println!("Enter the second number: ");
         let mut num2 = String::new();
         io::stdin().read_line(&mut num2).expect("Failed to read line");
-        let num2: f64 = num2.trim().parse().expect("Invalid number");
+        let num2: f64 = match num2.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number. Please enter a valid number.");
+                continue;
+            }
+        };
 
         match choice {
-            1 => println!("Result: {}", num1 + num2),
-            2 => println!("Result: {}", num1 - num2),
-            3 => println!("Result: {}", num1 * num2),
-            4 => {
-                if num2 == 0.0 {
-                    println!("Error: Division by zero.");
-                } else {
-                    println!("Result: {}", num1 / num2);
-                }
-            }
+            1 => println!("Result: {}", add(&num1, &num2)),
+            2 => println!("Result: {}", subtract(&num1, &num2)),
+            3 => println!("Result: {}", multiply(&num1, &num2)),
+            4 => println!("Result: {}", divide(&num1, &num2)),
             _ => println!("Invalid choice. Please select a valid operation."),
         }
+    }
+}
+
+fn add(a: &f64, b: &f64) -> f64 {
+    *a + *b
+}
+
+fn subtract(a: &f64, b: &f64) -> f64 {
+    *a - *b
+}
+
+fn multiply(a: &f64, b: &f64) -> f64 {
+    *a * *b
+}
+
+fn divide(a: &f64, b: &f64) -> f64 {
+    if *b == 0.0 {
+        println!("Error: Division by zero.");
+        std::f64::NAN
+    } else {
+        *a / *b
     }
 }
